d9f44f298b4969720f16ed90b99b18e64bf2500c
DaniiGomezz <gomezjesushd@gmail.com>
2024-05-02 15:59:33 -0300

    punto 5 terminado

--- debugging/servicio.js
+++ debugging/servicio.js
@@ -1,39 +1,43 @@
-import express from 'express'
-import cors from 'cors'
-import bodyParser from 'body-parser'
-const app = express()
-const port = 3000
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import path from 'path';
+
+const app = express();
+const port = 3000;
+
+// Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 app.use(cors());
 
-app.post('/sumar_matrices', (req, res) => {
-    try {
-      const { matrix1, matrix2 } = req.body;
-  
-      if (matrix1.length !== matrix2.length || matrix1[0].length !== matrix2[0].length) {
-        return res.status(400).send('Las matrices deben tener la misma dimensión');
-      }
-  
-      const rows = matrix1.length;
-      const cols = matrix1[0].length;
-  
-      let resultMatrix = new Array(rows);
-      for (let i = 0; i < rows; i++) {
-        resultMatrix[i] = new Array(cols);
-        for (let j = 0; j < cols; j++) {
-          resultMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
-        }
-      }
-  
-      res.json({ result: resultMatrix });
-    } catch (error) {
-      console.error('Error procesando la solicitud:', error);
-      res.status(500).send('Error interno del servidor');
-    }
-  });
+// Servir archivos estáticos desde la carpeta "public"
+app.use(express.static('public'));
 
-app.listen(port, () => {
-  console.log('Servidor escuchando en el puerto 3000');
+// Endpoint para sumar matrices
+app.post('/sumar_matrices', (req, res) => {
+  const { matrix1, matrix2 } = req.body;
+
+  if (matrix1.length === matrix2.length && matrix1[0].length === matrix2[0].length) {
+    const rows = matrix1.length;
+    const cols = matrix1[0].length;
+
+    let resultMatrix = new Array(rows);
+    for (let i = 0; i < rows; i++) {
+      resultMatrix[i] = new Array(cols);
+      for (let j = 0; j < cols; j++) {
+        resultMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
+      }
+    }
+
+    res.json({ result: resultMatrix });
+  } else {
+    res.status(400).send('Las matrices deben tener la misma dimensión');
+  }
 });
+
+
+  app.listen(port, async()=>{
+    console.log(`servidor corriendo en el puerto: http://localhost:${port}`);
+    
+})
